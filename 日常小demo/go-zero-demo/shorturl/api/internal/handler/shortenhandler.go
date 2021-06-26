be562c9cec1aeb3f15db59e6dfb9a27099e94401
sjw <2293261394@qq.com>
2021-06-26 13:51:54 +0800

    go-zero demo

---
+++ 日常小demo/go-zero-demo/shorturl/api/internal/handler/shortenhandler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"net/http"
+
+	"shorturl/api/internal/logic"
+	"shorturl/api/internal/svc"
+	"shorturl/api/internal/types"
+
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+//http://localhost:8888/shorten?url=http://www.xiaoheiban.cn
+func ShortenHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.ShortenReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+		//time.Sleep(time.Second * 10)
+		l := logic.NewShortenLogic(r.Context(), ctx)
+		resp, err := l.Shorten(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
---
+++ 日常小demo/判断当前系统的存储方式/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"unsafe"
+)
+
+//用Go判断，当前操作系统是大端存储还是小端存储
+func main() {
+
+	fn0()
+
+	//通过位运算即可实现
+	//大小端转换，这涉及到网络传输、文件存储，要保证读到的数据正确
+	BigEndianAndLittleEndianByLibrary()
+}
+
+func fn0() {
+	var a int64 = 1
+
+	//低32位
+	a1p := (*int32)(unsafe.Pointer(&a))
+	fmt.Println(*a1p)
+
+	//高32位
+	a2p := (*int32)(unsafe.Pointer(uintptr(unsafe.Pointer(&a)) + uintptr(unsafe.Sizeof(int32(1)))))
+	fmt.Println(*a2p)
+
+	//打印结果 1  0    明显是 低位字节放在低地址端，所以是小端存储
+	//大端存储：高低高低，也就是高位字节排放在内存低地址端，高地址端存在低位字节；
+	//小端存储：高高低低，也就是高位字节排放在内存的高地址端，低位字节排放在内存的低地址端；
+	/*
+		0x1A2B3C4D在大端与小端的表现形式
+
+		大端		0x0400		0x4001		0x4002		0x4003				小端 	0x0400		0x4001		0x4002		0x4003
+
+					 1A			  2B		  3C		 4D							 4D			  3C		  2B 		 1A
+	*/
+}
+
+// uint32 位的大小端转化	位运算
+func SwapEndianUint32(val uint32) uint32 {
+	return (val&0xff000000)>>24 | (val&0x00ff0000)>>8 |
+		(val&0x0000ff00)<<8 | (val&0x000000ff)<<24
+}
+
+//官方库
+func BigEndianAndLittleEndianByLibrary() {
+	var value uint32 = 10
+	bs := make([]byte, 4)
+	binary.BigEndian.PutUint32(bs, value)
+	fmt.Println("转化为大端之后的byte数组:", bs)
+	fmt.Println("使用大端转化输出之后的结果", binary.BigEndian.Uint32(bs))
+	little := binary.LittleEndian.Uint32(bs)		//将大端的数组转化为小端的值
+	fmt.Println("大端字节序使用小端输出结果：", little)
+}
