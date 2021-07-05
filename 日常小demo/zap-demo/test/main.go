9886b2c9a119509cc3f65da67359525de1b4559a
sjw <2293261394@qq.com>
2021-07-05 21:29:52 +0800

    stronc.AppendX()的功能

--- 日常小demo/zap-demo/test/main.go
+++ 日常小demo/zap-demo/test/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
-	"os"
+	"strconv"
 )
 
 func main() {
 
-	buf := bufio.NewWriter(os.Stdout)
-	buf.Write([]byte("123123"))
-	buf.Flush()
+	bs := []byte{}
+	bs = strconv.AppendInt(bs, 999, 10) //表示将999转为10进制的字符串，并且直接append到[]byte中
+
+	//看源码发现 0-99的数字，是通过硬编码的形式直接解析成string的
 }
